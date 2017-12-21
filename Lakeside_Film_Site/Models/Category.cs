07b2f080580d6ff8351d158dbfa5d06d97593dfc
Dawn Myers <progdawn@gmail.com>
2017-12-21 10:39:25 -0600

    Added Category and VM

---
+++ Lakeside_Film_Site/Models/Category.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.Mvc;
+
+namespace Lakeside_Film_Site.Models
+{
+    public class Category
+    {
+        [Required, Key]
+        public int Categoryid { get; set; }
+        [Required, MaxLength(20)]
+        public string CategoryName { get; set; }
+
+        public static IEnumerable<SelectListItem> GetCategoriesDDList(SqlConnection dbcon)
+        {
+            IList<SelectListItem> ddlist = new List<SelectListItem>();
+            string strsql = "select * from categories";
+            SqlCommand cmd = new SqlCommand(strsql, dbcon);
+            SqlDataReader myReader;
+            myReader = cmd.ExecuteReader();
+            while (myReader.Read())
+            {
+                ddlist.Add(new SelectListItem()
+                {
+                    Text = myReader["CategoryName"].ToString(),
+                    Value = myReader["Categoryid"].ToString()
+                });
+            }
+            myReader.Close();
+            return ddlist;
+        }
+    }
+}
---
+++ Lakeside_Film_Site/Models/ViewModels/FilmListVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lakeside_Film_Site.Models.ViewModels
+{
+    public class FilmListVM
+    {
+        public int selectedcatid { get; set; }
+        public List<Film> films { get; set; }
+        public IEnumerable<SelectListItem> catlist { get; set; }
+    }
+}
